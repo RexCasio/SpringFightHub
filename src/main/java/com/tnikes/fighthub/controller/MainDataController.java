0cd591ae5c6db5a952633a76aaa3fdb3493a00b9
Tegan <Tegan.Naicker@gmail.com>
2019-05-04 16:58:04 -0400

    Update controller endpoint values for spring security

--- src/main/java/com/tnikes/fighthub/controller/MainDataController.java
+++ src/main/java/com/tnikes/fighthub/controller/MainDataController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,15 +34,15 @@ public class MainDataController {
 	private INormalService normalService;
 	
 	//Default path 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/fighthub-api/", method = RequestMethod.GET)
 	public String defaultPath() {
 		
 		return "o hi there";
 	}
 	
 	//Path for the games. Only needs to have a get request since I don't want to be able to edit these values in the db from the api?
-	@RequestMapping(value = "/games", method = RequestMethod.GET)
- 	public List<Game> findGames(@RequestParam(value = "category", required = false) String category,
+	@RequestMapping(value = "/fighthub-api/games", method = RequestMethod.GET)
+ 	public ResponseEntity<List<Game>> findGames(@RequestParam(value = "category", required = false) String category,
  								@RequestParam(value = "gameName", required = false) String name) {
 		
 		//Checking if either parameters exist
@@ -62,13 +64,14 @@ public class MainDataController {
 			games = (List<Game>) gameService.findAllGames();
 		}
 		
-		return games;
+		return ResponseEntity.ok().body(games);
 	}
 	
 	//Path for the characters. There should probably be a separate path for game specified characters. EX: guiltygear/characters or guiltygear/characters?name=venom
 	//Again, do not need to run CUD of CRUD for this endpoint.
-	@RequestMapping(value = "/characters", method = RequestMethod.GET)
-	public List<GameCharacter> findCharacters(@RequestParam(value = "type", required = false) Integer gameId,
+	@RequestMapping(value = "/fighthub-api/characters", method = RequestMethod.GET)
+	@CrossOrigin(origins = "http://localhost:4200")
+	public ResponseEntity<List<GameCharacter>> findCharacters(@RequestParam(value = "type", required = false) Integer gameId,
 											@RequestParam(value = "name", required = false) String name) {
 		
 		//Checking if the gameid is provided. This should really be a name(String) that gets converted to the required id from a SQL search.
@@ -88,18 +91,18 @@ public class MainDataController {
 			gameCharacters = (List<GameCharacter>) characterService.findAllCharacters();
 		}
 		
-		return gameCharacters;
+		return ResponseEntity.ok().body(gameCharacters);
 	}
 	
 	//Path for the normal moves. As of right now this is ok since it's just pulling guilty gear data, but the sql statement needs to pull only the first result. Very brittle
 	//No point in making the request parameter optional, pulling in all of the normals unorganized makes 0 fucking sense.
 	//Need to do a join statement to pull in normals categorized into their respective character names in order to make the parameter optional.
 	//Path also needs to be something like '/{game}/{character}/normals
-	@RequestMapping(value = "/normals", method = RequestMethod.GET)
-	public List<Normals> findNormals(@RequestParam String name) {
+	@RequestMapping(value = "/fighthub-api/normals", method = RequestMethod.GET)
+	public ResponseEntity<List<Normals>> findNormals(@RequestParam String name) {
 		
 		List<Normals> normals = (List<Normals>) normalService.findNormalsByChar(name);
-		return normals;
+		return ResponseEntity.ok().body(normals);
 	}
 	
 }
--- src/main/java/com/tnikes/fighthub/repository/CharacterRepository.java
+++ src/main/java/com/tnikes/fighthub/repository/CharacterRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.tnikes.fighthub.model.GameCharacter;
 
 @Repository
+@CrossOrigin(origins = "http://localhost:4200")
 public interface CharacterRepository extends CrudRepository<GameCharacter, Long> {
 	
 	@Query(value = "SELECT * FROM characters;", nativeQuery = true)
